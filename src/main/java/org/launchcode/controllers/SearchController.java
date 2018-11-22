ba5ba01f65daa6ea60a5b9f85f5894f344208b56
Brian Quezada <briantpt30@gmail.com>
2018-11-21 21:53:09 -0500

    initial commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,12 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -23,5 +25,27 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        ArrayList<HashMap<String, String>> searchJobs;
 
+        if (searchType.equals("all")) {
+            searchJobs = JobData.findByValue(searchTerm);
+
+        } else {
+            searchJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("selectedColumn", searchType);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+
+        model.addAttribute("searchJobs", searchJobs);
+
+        return "search";
+    /* idk what to do. i built this to handle the search. do i need to make another handler to pass
+    this function above too? to display the html form?
+    not sure what to do next. also the html could be wrong.
+     */
+    }
 }
